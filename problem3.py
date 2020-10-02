42c09904ee4bf1e3409a639813b63f17484135b5
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-02 12:50:25 -0700

    .

--- problem3.py
+++ problem3.py
@@ -4,7 +4,7 @@ a=input("Enter a number smaller than 10: ")
 a=int(a)
 
 n=1
-print("The sum of the series is ", end="")
+print("the sum of the series is ", end="")
 for i in range(1,a+1):
     print(n,end="")
     n=n+1
