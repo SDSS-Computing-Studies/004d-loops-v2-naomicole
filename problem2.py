e901537ef8197b7431c14f984bb32334eda9614a
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-02 12:38:05 -0700

    .

--- problem2.py
+++ problem2.py
@@ -2,7 +2,11 @@
 
 n=input("Enter a number: ")
 n=int(n)
-t=n
-s=t-1
+b=0
+c=1
 
-for i in range()
+for i in range(0,n):
+    b=n-i
+    c=b*c
+
+print(str(n)+"! is "+str(c)+ " where "+str(n)+" is the integer entered and "+str(c)+ " is the calculated answer")
--- problem3.py
+++ problem3.py
@@ -3,5 +3,10 @@
 a=input("Enter a number smaller than 10: ")
 a=int(a)
 
+n=0
+
 for i in a:
+    if n<=a:
+        n=n+1
+        print(n)
     
